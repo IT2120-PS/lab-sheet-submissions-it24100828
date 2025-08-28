59aca778d1a5795f0ddb69b23330472201fd1fdc
it24100828 <IT24100828@my.sliit.lk>
2025-08-28 14:16:02 +0530

    lab05

---
+++ Lab Sheet 05/it24100828ps05.R
@@ -0,0 +1,15 @@
+setwd("C://Users//it24100828//Desktop//IT24100828PS5")
+Delivery_Times <- read.table("Exercise - Lab 05.txt", header = TRUE)
+Delivery_Times$`Delivery_Time_.minutes.` <- as.numeric(Delivery_Times$`Delivery_Time_.minutes.`)
+hist(Delivery_Times$`Delivery_Time_.minutes.`,
+     breaks = seq(20, 70, by = 5),
+     right = TRUE,
+     main = "Histogram of Delivery Times",
+     xlab = "Delivery Time",
+     ylab = "Frequency")
+cf <- cumsum(table(cut(Delivery_Times$`Delivery_Time_.minutes.`, breaks = seq(20, 70, by = 5), right = TRUE)))
+plot(seq(22.5, 67.5, by = 5), cf, type = "o",
+     xlab = "Delivery Time", ylab = "Cumulative Frequency",
+     main = "Cumulative Frequency Polygon (Ogive)")
+
+
